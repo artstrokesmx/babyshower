a089addf9455833fd22e238b80c18d7c6c850926
Miguel Arturo García Miranda <psic.arturomiranda@gmail.com>
2025-10-14 20:21:58 -0600

    cambio de letra mesa de regalos

--- src/componentes/Mesa/Mesa.jsx
+++ src/componentes/Mesa/Mesa.jsx
@@ -5,7 +5,7 @@ return (
         <div className=" bg-white w-90 mx-auto p-5 rounded-lg">
             <p className="general letragirly">Lo más importante para nosotros, es compartir la alegría de la espera con ustedes.</p>
             <p className="general letragirly mb-5">Y si, además deseas apoyarnos, puedes hacerlo a través de nuestra mesa de regalos en:</p>
-            <a className="text-blue-300" href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51717141" target="_blank" rel="noopener noreferrer">
+            <a className="text-blue-600" href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51717141" target="_blank" rel="noopener noreferrer">
                 <img className="rounded-2xl w-30 mx-auto" src="/babyshower/assets/liverpool.png" alt="" />
                 <p>(Da click para ir a ella)</p>
             </a>
