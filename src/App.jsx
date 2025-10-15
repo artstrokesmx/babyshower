b9615b45016e214e07186490e61358db66304225
Miguel Arturo García Miranda <psic.arturomiranda@gmail.com>
2025-10-14 19:19:33 -0600

    bottom o en cuna

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
   return (
     <div className="fondo overflow-hidden ">
       <div className=" inset-0 pointer-events-none">
-        <img className="w-20 translate-x-1 translate-y-158 z-1 fixed" src="/babyshower/assets/cuna.png" alt="Marco inferior izquierdo" />
+        <img className="w-20 translate-x-1 translate-y-158 z-1 fixed bottom-0" src="/babyshower/assets/cuna.png" alt="Marco inferior izquierdo" />
         <img className="w-20 translate-x-85 translate-y-165 fixed" src="/babyshower/assets/elev.png" alt="Marco inferior derecha" />
         <img className="w-20 fixed" src="/babyshower/assets/globo.png" alt="Marco inferior izquierdo" style={{transform: `translate(${307}px, ${660 - offsetY * 0.275}px)`}} />
         <img className="w-22 fixed" src="/babyshower/assets/ciguena.png" alt="Marco inferior izquierdo" style = {{transform: `translate(${-25 + offsetY * 0.16}px,${-40}px)`}} />
